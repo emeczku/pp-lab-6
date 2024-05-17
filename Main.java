125307352dcbab278acaeeeee5e8c3985b20039e
Przemek Majewski <dzius89@gmail.com>
2024-05-17 20:08:28 +0200

    extend Employee class with additional fields and update Main class

--- Main.java
+++ Main.java
@@ -1,23 +1,29 @@
 import company.models.Worker;
 import company.models.Manager;
+import company.abstracts.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Tom", 3000, 1);
-        Worker worker2 = new Worker("Steven", 3200, 2);
-        Worker worker3 = new Worker("Bob", 3100, 3);
-        Worker worker4 = new Worker("Mat", 3300, 4);
+        Worker worker1 = new Worker("Tom", 3000, 1, "2023-01-15", "Developer");
+        Worker worker2 = new Worker("Steven", 3200, 2, "2023-03-22", "Tester");
+        Worker worker3 = new Worker("Bob", 3100, 3, "2023-05-30", "Designer");
+        Worker worker4 = new Worker("Mat", 3300, 4, "2023-07-19", "Developer");
 
-        Manager manager = new Manager("Daniel", 5000, 5);
+        Manager manager = new Manager("Daniel", 5000, 5, "2023-11-11", "Project Manager");
 
-        Worker[] workers = { worker1, worker2, worker3, worker4 };
+        List<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
 
-        for (Worker worker : workers) {
-            System.out.println("Worker salary: " + worker.getSalary());
-            worker.work();
+        for (Employee employee : employees) {
+            employee.work();
+            System.out.println("-" + employee.getName() + " (ID: " + employee.hashCode() + ", Position: " + employee.getPosition() + ", Hire date: " + employee.getHireDate() + ", Salary: " + employee.getSalary() + ").");
         }
-
-        System.out.println("Manager salary: " + manager.getSalary());
-        manager.work();
     }
 }
